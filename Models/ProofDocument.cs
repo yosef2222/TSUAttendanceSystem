7d729287b378a7ec38115a2bda520a13bce6e431
Yusuf Osama <yosef.eldesoky@gmail.com>
2025-03-03 16:01:29 +0700

    added a folder

--- Models/ProofDocument.cs
+++
@@ -1,9 +0,0 @@
-namespace AttendanceTracker.Models;
-
-public class ProofDocument
-{
-    public int Id { get; set; }
-    public int AbsenceRequestId { get; set; }
-    public AbsenceRequest AbsenceRequest { get; set; }
-    public string FilePath { get; set; }
-}
