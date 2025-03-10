eca1fcdc36adbe8763d3cf902831d19922faaea1
Pavel Fomin <pashaksvp@mail.ru>
2025-03-10 16:42:12 +0700

    validation

--- TSUAttendanceSystem/TSUAttendanceSystem/obj/Debug/net8.0/TSUAttendanceSystem.AssemblyInfo.cs
+++ TSUAttendanceSystem/TSUAttendanceSystem/obj/Debug/net8.0/TSUAttendanceSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TSUAttendanceSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c7a09a51a1f3236879a6c42e2d5b293d093d704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0243a4b004ae15074ea7ebf71e37a94da6bf658f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TSUAttendanceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TSUAttendanceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
